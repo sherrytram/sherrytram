cc08003e21a1b7081969f6edec1d25384e3e9d06
Sherry Tram <sherrymtram@gmail.com>
2021-03-11 18:51:02 -0800

    welcome fade, toggle opacity&text when hover

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-<<<<<<< HEAD
-=======
-# sherrytram.github.io
->>>>>>> d7fec61f3d2900b4c8d2244f09a5053b5ddf4932
+Sherry Tram - Personal Portfolio Site
--- my.js
+++ my.js
@@ -1,3 +1,37 @@
 $(document).ready(function(){
     $("div.fadeIn").fadeIn(1500).removeClass('hidden');
+
+    $("p.imgtext1").hide();
+    $("p.imgtext2").hide();
+    $("p.imgtext3").hide();
+
+    $("a.navbutton1").mouseover(function() {
+        $("img.img1").animate({opacity: 0.5}, 200);
+        $("p.imgtext1").show();
+    });
+    $("a.navbutton1").mouseout(function() {
+        $("img.img1").animate({opacity: 1}, 200 );
+        $("p.imgtext1").hide();
+    });
+
+    $("a.navbutton2").mouseover(function() {
+        $("img.img2").animate({opacity: 0.5}, 200);
+        $("p.imgtext2").show();
+    });
+    $("a.navbutton2").mouseout(function() {
+        $("img.img2").animate({opacity: 1}, 200 );
+        $("p.imgtext2").hide();
+    });
+
+    $("a.navbutton3").mouseover(function() {
+        $("img.img3").animate({opacity: 0.5}, 200);
+        $("p.imgtext3").show();
+    });
+    $("a.navbutton3").mouseout(function() {
+        $("img.img3").animate({opacity: 1}, 200 );
+        $("p.imgtext3").hide();
+    });
+    
+  
+   
 });
